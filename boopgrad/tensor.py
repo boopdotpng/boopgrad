b43d1f276456245bb42e4372745139974cbd40ee
anuraag <anuraag.warudkar@gmail.com>
2024-08-23 20:32:50 -0400

    added packages for metal backend

--- boopgrad/tensor.py
+++ boopgrad/tensor.py
@@ -5,10 +5,17 @@ class Tensor:
         self.data = data
         self.grad = None
         self._graph = None
+    # tensor operations are not evaluated until you call realize()
     def realize():
         pass
-    def detach():
-        pass
-    @staticmethod()
+    # return a new tensor, detached from autograd graph
+    def detach(self): return Tensor(self.data)
+    # ** methods that generate new tensors ** 
+    @staticmethod
+    # a tensor of all zeros
     def zeros(*dims):
+        pass
+    @staticmethod
+    # the default way to initialize linear layers
+    def kaiming_uniform(*dims):
         pass
